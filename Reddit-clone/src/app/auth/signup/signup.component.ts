c6549689d7e6233e6bf9c09ddc07483b41673966
KrzysztofDolanski <k.dolanski@wp.pl>
2020-11-04 10:36:28 +0100

    Signup form

---
+++ Reddit-clone/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request-payload';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup;
+
+
+  constructor() {
+    this.signupRequestPayload = {
+      username: '',
+      password: '',
+      email: ''
+    }
+   }
+
+
+
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+
+    })
+  }
+
+}
