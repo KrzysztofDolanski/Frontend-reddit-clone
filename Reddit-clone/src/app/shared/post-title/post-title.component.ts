144632258a0d1ae5c1bce567f9dff32c2fcae2e1
KrzysztofDolanski <k.dolanski@wp.pl>
2020-11-07 17:08:59 +0100

    post, vote refactor

--- Reddit-clone/src/app/shared/post-title/post-title.component.ts
+++ Reddit-clone/src/app/shared/post-title/post-title.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostModel } from '../post-model';
 import {PostService} from '../post.service';
 import {faArrowUp, faArrowDown, faComments} from '@fortawsome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-title',
@@ -15,14 +16,19 @@ export class PostTitleComponent implements OnInit {
   
 
   faComments = faComments;
+  @Input() posts: PostModel[];
 
-  constructor(private postService: PostService) {
+  constructor(private postService: PostService, private router: Router) {
     this.postService.getAllPosts().subscribe(post=> {
       this.posts$ = post;
     });
    }
 
   ngOnInit(): void {
+  }
+
+  goToPost(id: number): void {
+    this.router.navigateByUrl('/view-post' + id);
   }
 
 }
